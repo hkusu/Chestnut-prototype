9f3ca85624ba3ac24bd4a422f444cc6236f5f4dd
Hiroyuki Kusu <kusu0806@gmail.com>
2024-09-29 12:16:31 +0900

    Coroutines導入

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -26,6 +26,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
+            implementation(libs.coroutines.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
