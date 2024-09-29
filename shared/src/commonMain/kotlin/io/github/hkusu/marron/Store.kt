800528c3e1accaa5b542c936785f3718d32de242
Hiroyuki Kusu <kusu0806@gmail.com>
2024-09-29 20:44:02 +0900

    改修

--- shared/src/commonMain/kotlin/io/github/hkusu/marron/Store.kt
+++ shared/src/commonMain/kotlin/io/github/hkusu/marron/Store.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -12,35 +13,60 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-// ViewModel で保持
+// ViewModel で保持（ KMP プロジェクトでない場合は ViewModel に implement してもよし
 class MainStore(
-    coroutineScope: CoroutineScope,
+    // UseCase や Repository を inject
+    //   getHogeListUseCase: GetHogeListUseCase,
+    //   setgFugaUseCase: SetFugaUseCase,
+    coroutineScope: CoroutineScope, // 基本は viewModelScope を渡す想定
 ) : Store<MainState, MainAction, MainEvent>(
     initialState = MainState.Initial,
+    enterAction = MainAction.Enter,
+    exitAction = MainAction.Exit,
     coroutineScope = coroutineScope,
 ) {
-    override suspend fun getState(state: MainState, action: MainAction, emmit: EventEmmit<MainEvent>): MainState {
+    override suspend fun onDispatched(state: MainState, action: MainAction, emmit: EventEmmit<MainEvent>): MainState? {
         return when (state) {
-            is MainState.Loading -> {
+            MainState.Initial -> {
                 when (action) {
-                    MainAction.OnClicked -> {
-                        state
+                    MainAction.Enter -> {
+                        // すぐさま Loading に
+                        MainState.Loading
                     }
 
-                    MainAction.OnLoaded -> {
-                        state
-                    }
+                    else -> null
                 }
             }
 
-            MainState.Initial -> {
+            MainState.Loading -> {
                 when (action) {
-                    MainAction.OnClicked -> {
-                        emmit(MainEvent.OnLoadedClicked)
-                        state
+                    MainAction.Enter -> {
+                        // UseCase や Repository からデータ取得
+                        delay(5_000)
+                        // データを読み終わったら Stable に
+                        MainState.Stable(listOf())
                     }
 
-                    else -> state
+                    else -> null
+                }
+            }
+
+            is MainState.Stable -> {
+                when (action) {
+                    MainAction.Enter -> {
+                        // Compose で state.dataList のデータを画面へ描画する
+
+                        // イベント発行例
+                        emmit(MainEvent.ShowToast("データがロードされました"))
+                        null
+                    }
+
+                    is MainAction.Click -> {
+                        // state の更新は data class の copy で
+                        state.copy(clickCounter = state.clickCounter + 1)
+                    }
+
+                    else -> null
                 }
             }
         }
@@ -49,22 +75,39 @@ class MainStore(
 
 abstract class Store<S : State, A : Action, E : Event>(
     initialState: S,
+    private val enterAction: A?,
+    private val exitAction: A?,
     private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
 ) {
     private val _state: MutableStateFlow<S> = MutableStateFlow(initialState)
-    val state = _state.asStateFlow()
+    val state = _state.asStateFlow() // Compose で購読
 
     private val _event: MutableSharedFlow<Event> = MutableSharedFlow()
-    val event = _event.asSharedFlow()
+    val event = _event.asSharedFlow() // Compose で購読
 
-    protected abstract suspend fun getState(state: S, action: A, emmit: EventEmmit<E>): S
+    protected abstract suspend fun onDispatched(state: S, action: A, emmit: EventEmmit<E>): S?
 
-    fun dispatch(action: A) {
+    init {
+        enterAction?.let { dispatch(it) }
+    }
+
+    fun dispatch(action: A) { // ユーザによる操作. Compose の画面から叩く
+        val prevState = _state.value
         coroutineScope.launch {
-            _state.update {
-                getState(_state.value, action) {
-                    _event.emit(it)
-                }
+            val nextState = onDispatched(prevState, action) {
+                _event.emit(it)
+            } ?: return@launch
+
+            if (action == exitAction) return@launch
+
+            if (prevState::class.qualifiedName != nextState::class.qualifiedName) {
+                exitAction?.let { dispatch(it) }
+            }
+
+            _state.update { nextState }
+
+            if (prevState::class.qualifiedName != nextState::class.qualifiedName) {
+                enterAction?.let { dispatch(it) }
             }
         }
     }
@@ -79,20 +122,27 @@ abstract class Store<S : State, A : Action, E : Event>(
     }
 }
 
-sealed interface State
-sealed interface Action
-sealed interface Event
+sealed interface Contract
+
+sealed interface State : Contract
+sealed interface Action : Contract
+sealed interface Event : Contract
 
 sealed interface MainState : State {
-    data class Loading(val hoge: String) : MainState
     data object Initial : MainState
+    data object Loading : MainState
+    data class Stable(
+        val dataList: List<String>,
+        val clickCounter: Int = 0,
+    ) : MainState
 }
 
 sealed interface MainAction : Action {
-    data object OnClicked : MainAction
-    data object OnLoaded : MainAction
+    data object Enter : MainAction
+    data object Exit : MainAction
+    data class Click(val id: Long) : MainAction
 }
 
 sealed interface MainEvent : Event {
-    data object OnLoadedClicked : MainEvent
+    data class ShowToast(val message: String) : MainEvent
 }
